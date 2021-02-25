e27b1af1e5010e7c925e953701256dfa240be6a8
CharlieCharlie79 <carloswork067@hotmail.com>
2021-02-25 01:30:16 -0500

    Enemy_Follows_Player

---
+++ Capstone2021_Final/Assets/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+
+    Transform target;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            if(distance <= agent.stoppingDistance)
+            {
+
+                //Face target
+                FaceTarget();
+            }
+        }
+    }
+
+    void FaceTarget()
+    {
+        //Direction of the target
+        Vector3 direction = (target.position - transform.position).normalized;
+        //Rotation when pointing to that target
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        //Update rotation to point to that direction
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
