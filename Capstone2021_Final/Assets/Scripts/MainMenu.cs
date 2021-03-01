b7b4e219e8516e36dcdea5f94deba48b307928a5
CharlieCharlie79 <carloswork067@hotmail.com>
2021-03-01 14:33:45 -0500

    Fade_Effect

--- Capstone2021_Final/Assets/Scripts/MainMenu.cs
+++ Capstone2021_Final/Assets/Scripts/MainMenu.cs
@@ -5,9 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transition;
+
+    public float transitionTime = 2f;
+
     public void  PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
+
+        //Load animation with Scene
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+       
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Play animation
+        transition.SetTrigger("Strat");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load scene
+        SceneManager.LoadScene(levelIndex);
     }
 
     public void QuitGame()
