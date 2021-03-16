a7662129d49a6d06b275e7c82baf043ce6cf94d9
CharlieCharlie79 <carloswork067@hotmail.com>
2021-03-15 20:01:21 -0400

    Level_1_Loaded

---
+++ Capstone2021_Final/Assets/Scripts/Level_Loader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Loader : MonoBehaviour
+{
+
+     void OnCollisionEnter(Collision coll)
+    {
+        if(coll.collider.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Level_1");
+        }
+    }
+}
