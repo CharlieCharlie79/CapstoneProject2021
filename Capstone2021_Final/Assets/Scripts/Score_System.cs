0fe9246e0acc8328a151cb183efcbc3ce7870182
CharlieCharlie79 <carloswork067@hotmail.com>
2021-03-25 16:18:07 -0400

    Details_Fix

--- Capstone2021_Final/Assets/Scripts/Score_System.cs
+++ Capstone2021_Final/Assets/Scripts/Score_System.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class Score_System : MonoBehaviour
 {
     public static int scoreValue = 0;
@@ -18,6 +18,15 @@ public class Score_System : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        score.text = "" + scoreValue;        
+        score.text = "" + scoreValue;  
+        
+        if(scoreValue >= 50)
+        {
+            SceneManager.LoadScene("Victory_Scene");
+            
+            Cursor.lockState = CursorLockMode.Confined; //Locks the mouse
+            Cursor.visible = true; // Make the cursor visable
+            
+        }
     }
 }
